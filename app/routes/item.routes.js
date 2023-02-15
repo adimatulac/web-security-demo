5b7f133bf8abac3cae0a2c8d6d808284de2afb4b
adimatulac <angellidimatulac@gmail.com>
2023-02-15 12:59:06 -0800

    added new items + refactoring

--- app/routes/item.routes.js
+++ app/routes/item.routes.js
@@ -2,6 +2,6 @@ module.exports = (app) => {
   const items = require("../controllers/item.controller.js");
 
   app.post("/items", items.add);
-  // app.get("/items", items.fetchAll);
+  app.get("/items", items.fetchAll);
   // app.get("/items/random", items.fetchRandom)
 };
